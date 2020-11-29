52781dc8b83869d7e8d6db7e7d0a0c9b318650e2
mhmdreda99 <Moreda491999@gmail.com>
2020-11-29 15:01:50 +0200

    from 30 to 70 array

---
+++ Task2/array_from30t70.py
@@ -0,0 +1,7 @@
+#Write a NumPy program to create an array of the integers from 30 to70.
+import numpy as np
+arr=np.arange(30,71)
+print("creating an array of the integers from 30 to 70:")
+print(arr)
+
+# print( len(arr))
