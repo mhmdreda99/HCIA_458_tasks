0f3e7c9cb9584733490d9935ab72f59c5eb17e48
mhmdreda99 <Moreda491999@gmail.com>
2020-11-29 15:07:12 +0200

    push this

---
+++ Task2/vector_fro15to55.py
@@ -0,0 +1,6 @@
+import numpy as np
+vect = np.arange(15,55)
+print("creating vector in range of 15 to 55:\n")
+print(vect)
+print("\nAll values except the first and last of the said vector:\n")
+print(vect[1:-1])
